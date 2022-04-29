15a4edddac547325bd79a70f964048c62bcdca34
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-29 11:11:47 +0200

    feat: basic moving algorithm

--- Demenagement/src/Main.java
+++ Demenagement/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
+        int firstLocalContent = 34;
+        int truckContent = 0;
+        int truckMaxCapacity = 9;
+        int newLocalContent = 0;
+
+        while (firstLocalContent > 0) {
+            if (firstLocalContent >= truckMaxCapacity) {
+                firstLocalContent -=truckMaxCapacity;
+                truckContent +=truckMaxCapacity;
+                System.out.println("Un voyage de " + truckContent + " cartons");
+                newLocalContent +=truckContent;
+                truckContent -=truckContent;
+            } else {
+                truckContent +=firstLocalContent;
+                firstLocalContent -=firstLocalContent;
+                System.out.println("Un voyage de " + truckContent + " cartons");
+                newLocalContent +=truckContent;
+                truckContent -=truckContent;
+            }
+        }
+        System.out.println("Fin du déménagement. Le premier local contient " + firstLocalContent + " cartons. Le camion contient " + truckContent + " cartons. Le nouveau local contient " + newLocalContent + " cartons");
+
     }
 }
